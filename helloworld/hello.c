01a9255f05f5e44fbf4fd4315f8d34d4b46a7644
jason_yao <jason_yao@htc.com>
2014-06-18 22:15:35 +0800

    update

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(int argc, char** argv) {
-    printf("Hello World\n");
+    int i;
+
+    for ( i = 0; i < 10; i++ ) {
+        printf("Hello World\n");
+    }
+
     return 0;
 }
